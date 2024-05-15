dd3986c0b5385b88da66faf4636dfb1534b1c53b
Rugren <rubgarciaruiz@gmail.com>
2024-05-15 13:09:02 +0200

    Creado pipe formatValueImage (para dar valor al formato del nombre de la imagen)
    Archivos pipe recién creados:
    ng g pipe pipes/formatValueImage

---
+++ tienda/dashboard/src/app/pipes/format-value-image.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatValueImagePipe } from './format-value-image.pipe';
+
+describe('FormatValueImagePipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatValueImagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
