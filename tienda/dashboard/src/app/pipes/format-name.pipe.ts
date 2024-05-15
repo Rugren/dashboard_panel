642aa17d1ed688d9cc26f85d53f6fd2041c1f415
Rugren <rubgarciaruiz@gmail.com>
2024-05-15 13:06:33 +0200

    Función primera letra mayúscula. Y en el nombre de los campos aparezcan un renombre o traducido
    - En el nombre de los campos aparezcan un renombre o traducido, y no se vea el del código.
    
    - Función para que muestre la primera letra en mayúscula en los títulos de la tabla.

--- tienda/dashboard/src/app/pipes/format-name.pipe.ts
+++ tienda/dashboard/src/app/pipes/format-name.pipe.ts
@@ -5,8 +5,100 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatNamePipe implements PipeTransform {
 
+  /*
   transform(value: unknown, ...args: unknown[]): unknown {
     return null;
-  }
+  } 
+  */
+
+  transform(value: string): string {
+    /* Que me detecte la posición 0 de esa palabra, me la ponga en mayúscula 
+    y que me corte en la palabra 1 con value.slice(1)
+    es decir, que muestre solo la primera palabra en mayúscula 
+    let newValue = value.charAt(0).toUpperCase()+value.slice(1)
+    return newValue;
+    */
+
+    /* Para que en la web, en vez de poner imageUrls aparezca el nombre en la casilla de "Imagen" 
+    y no se vean los campos tal cual nombrados en programación. */
+    if(value === "imageUrls") {
+      return 'Imagen'
+    }
+
+    if(value === "more_description") {
+      return 'Más descripción'
+    }
+
+    /* este no hace falta traducirlo 
+    if(value === "stock") {
+      return 'Stock'
+    } */
+
+    if(value === "isBestSeller") {
+      return 'Más vendido'
+    }
+
+    if(value === "isFeatured") {
+      return 'Presentado'
+    }
+
+    if(value === "isNewArrival") {
+      return 'Lo nuevo'
+    }
+
+    if(value === "isSpecialOffer") {
+      return 'Oferta'
+    }
+
+    if(value === "precio_venta") {
+      return 'Precio Venta'
+    }
+
+    if(value === "precio_regular") {
+      return 'Precio regular'
+    }
+
+    if(value === "slug") {
+      return 'URL'
+    }
+
+    if(value === "brand") {
+      return 'Marca'
+    }
+
+    if(value === "currency") {
+      return 'Moneda'
+    }
+
+    if(value === "status") {
+      return 'Estado'
+    }
+
+    if(value === "availability") {
+      return 'Disponibilidad'
+    }
+
+    if(value === "updated_at") {
+      return 'Actualizado a'
+    }
+
+    if(value === "created_at") {
+      return 'Creado a'
+    }
+
+
+    // 1.0 Que si hay una barra baja
+    let newValueArray: any = value.split('_')
+    // .map para mapearlo 
+    newValueArray = newValueArray.map( (nombre: string) => nombre.charAt(0).toUpperCase()+nombre.slice(1) )
+    // 1.1 Nos la sustituya por espacios
+    let newValue = newValueArray.join(' ')
+    return newValue;
+
+
+
+  } 
+
+
 
 }
