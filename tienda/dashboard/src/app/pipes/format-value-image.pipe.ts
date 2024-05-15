6b831d50986ac01d2ddad9d1abd5d7118ae18b78
Rugren <rubgarciaruiz@gmail.com>
2024-05-15 13:26:23 +0200

    Para que nos muestre las imágenes con el FormatValueImagePipe

--- tienda/dashboard/src/app/pipes/format-value-image.pipe.ts
+++ tienda/dashboard/src/app/pipes/format-value-image.pipe.ts
@@ -5,8 +5,21 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatValueImagePipe implements PipeTransform {
 
+  /*
   transform(value: unknown, ...args: unknown[]): unknown {
     return null;
+  }
+  */
+
+  transform(value: any, nombre: any): any {
+    let newValue = value;
+    if (nombre == "imageUrls") {
+      // que nos rescate la posición primera, para que no nos muestre el nombre de todas las imágenes.
+      const url = value[0]
+      // Hay que usar comillas invertidas (backticks) para poder interpolar variables dentro de una cadena de texto en JavaScript
+      newValue = `<img src="${url}" width="50px" height="50px"/>`;
+    }
+    return newValue;
   }
 
 }
