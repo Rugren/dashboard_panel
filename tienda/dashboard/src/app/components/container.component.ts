cd66d1ccbad72def3040b4ce831cc600445e2af5
Rugren <rubgarciaruiz@gmail.com>
2024-05-16 12:18:14 +0200

    Pocisionamiento de la Paginación - Todo OK
    - Método para empezar y terminar la paginación
    
    Faltaría por mejorar que en cada página que avancemos nos cuente el nº de productos por dónde se quedó en el anterior.
    
    https://github.com/michaelbromley/ngx-pagination

--- tienda/dashboard/src/app/components/container.component.ts
+++ tienda/dashboard/src/app/components/container.component.ts
@@ -72,4 +72,64 @@ export class ContainerComponent implements OnInit {
     return data[index]
   }
 
+  /* Función que nos refleja en la página dónde estamos la continuación de nº de productos por ejemplo, 
+  que ponga productos 6-10, en lugar de que cuente de nuevo 1-5. (en la páginación) */
+  /* 
+  // 1.1 Este método es para empezar la paginación (Así es como se hizo en el tutorial)
+  getStartIndex(currentPage: number, lastPage: number) {
+    let firstIndex = 1;
+    if((currentPage !== lastPage) || (currentPage > 0 && lastPage > 0)) {
+      firstIndex = (Number(this.noOfRows) * (Number(currentPage) -1 ) + 1 )
+      // Este -1 y +1 es para que no aparezca más allá de la 1ª pág y última.
+    }
+    return firstIndex.toString();
+  }
+
+  // ASÍ NO FUNCIONA cuando es la última página.
+  // 1.2 (no funciona) Este método es para terminar la paginación (Así es como se hizo en el tutorial)
+  getLastIndex(currentPage: number, lastPage: number) {
+    let lastIndex = this.datas ? this.datas.lenght : null;
+    if((currentPage !== lastPage)) {
+      lastIndex = (Number(this.noOfRows) * (Number(currentPage)))
+    }
+    return lastIndex.toString();
+  } 
+  
+  // 1.2+- (funciona a medias, suma en la última página) Este método es para terminar la paginación
+  getLastIndex(currentPage: number, lastPage: number) {
+  let lastIndex = null;
+  if (currentPage === lastPage) {
+    lastIndex = (Number(this.noOfRows) * (Number(currentPage) - 1)) + (this.datas ? this.datas.length : 0);
+  } else if (currentPage < lastPage) {
+    lastIndex = (Number(this.noOfRows) * Number(currentPage));
+  }
+  return lastIndex.toString();
+  }
+  */
+  
+  // ASÍ ES OTRA MANERA, SÍ FUNCIONAN AMBOS:
+  // 2.1 Este método es para empezar la paginación
+  getStartIndex(currentPage: number, lastPage: number) {
+    let firstIndex = 1;
+    if (currentPage > 0 && currentPage <= lastPage) {
+      firstIndex = (this.noOfRows * (currentPage - 1)) + 1;
+      // Este -1 y +1 es para que no aparezca más allá de la 1ª pág y última.
+    }
+    return firstIndex.toString();
+  }
+
+  // 2.2 Este método es para terminar la paginación
+  getLastIndex(currentPage: number, lastPage: number) {
+    let lastIndex = null;
+    if (currentPage > 0 && currentPage < lastPage) {
+      lastIndex = Math.min(this.noOfRows * currentPage, this.datas ? this.datas.length : 0);
+    } else if (currentPage === lastPage) {
+      lastIndex = this.datas ? this.datas.length : null;
+    }
+    return lastIndex.toString();
+  }
+
+
+
+
 }
