fd40cbf9a3619c4a0ebe565baf5542b163adc628
Rugren <rubgarciaruiz@gmail.com>
2024-05-23 14:48:14 +0200

    Ir viendo si pone data._id yo reemplazar por data.uid
    - Desde el commit anterior al del comentario, en adelante ir viendo que dónde él ponga data._id y yo poner data.uid

--- tienda/dashboard/src/app/components/data-admin/data-admin.component.ts
+++ tienda/dashboard/src/app/components/data-admin/data-admin.component.ts
@@ -8,3 +8,29 @@ import { Component } from '@angular/core';
 export class DataAdminComponent {
 
 }
+
+/* por si lo necesitamos para poner algo similar en el tutorial más adelante
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-data-admin',
+  templateUrl: './data-admin.component.html',
+  styleUrl: './data-admin.component.css'
+})
+export class DataAdminComponent implements OnInit {
+  entity: string | undefined;
+  id: string | undefined;
+  action: string | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.entity = params['entity'];
+      this.id = params['id'];
+      this.action = params['action'];
+      console.log(`Entity: ${this.entity}, ID: ${this.id}, Action: ${this.action}`);
+    });
+  }
+}*/
