b14fbd725763341c2365b43bdf185ab9ff98b977
Rugren <rubgarciaruiz@gmail.com>
2024-05-27 14:38:05 +0200

    Modificado componente imageView (para mostrar la imagen)
    - Modificado componente imageView (para mostrar la imagen).
    Ya se ve cuando hacemos click en ver o editar un producto.

--- tienda/dashboard/src/app/components/image-view/image-view.component.ts
+++ tienda/dashboard/src/app/components/image-view/image-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-view',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './image-view.component.css'
 })
 export class ImageViewComponent {
+
+  @Input() imageUrls: any = [];
 
 }
