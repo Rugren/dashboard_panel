caebc700f9220f1f15276355118f41b23ae3a49b
Rugren <rubgarciaruiz@gmail.com>
2024-05-27 13:20:24 +0200

    dataId en el formulario (para editar el contenido del texto).
    - Añadiendo la data (dataId) en el formulario (para poder editar el contenido del texto).

--- tienda/dashboard/src/app/pipes/format-form-value.pipe.ts
+++ tienda/dashboard/src/app/pipes/format-form-value.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatFormValuePipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(nombre: any, dataId: any): any {
+    // retornamos los valores que encuentre en nombre
+    return dataId[nombre];
   }
 
 }
+
+
+
