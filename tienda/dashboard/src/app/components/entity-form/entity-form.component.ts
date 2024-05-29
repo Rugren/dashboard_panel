3468878f08468fffcf88c0ad5e47551d8a89cfdc
Rugren <rubgarciaruiz@gmail.com>
2024-05-29 14:35:17 +0200

    Eliminando campos+++
    - Eliminando campos para que no aparezcan en la web los campos que elijamos que no se muestren.+++

--- tienda/dashboard/src/app/components/entity-form/entity-form.component.ts
+++ tienda/dashboard/src/app/components/entity-form/entity-form.component.ts
@@ -1,13 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-entity-form',
   templateUrl: './entity-form.component.html',
   styleUrl: './entity-form.component.css'
 })
-export class EntityFormComponent {
+export class EntityFormComponent implements OnInit {
 
   @Input() entityNames: Array<any> = []
   @Input() dataId: any;
+
+  constructor() { }
+  
+  ngOnInit(): void {
+
+    // ANOTAR AQUÍ LOS CAMPOS QUE NO QUERAMOS QUE NOS MUESTRE:
+    this.entityNames = this.entityNames.filter((nombre: string) => {
+      // Si contiene el campo 'created_at', que no nos lo muestre.
+      if (nombre === 'created_at') {
+        return false;
+      }
+      // Si contiene el campo 'updated_at', que no nos lo muestre.
+      if (nombre === 'updated_at') {
+        return false;
+      }
+
+      return true;
+    })
+    
+  }
 
 }
